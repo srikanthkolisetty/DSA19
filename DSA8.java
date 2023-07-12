8ba5cb7e2967dffb68f85a9c5be38c43af1325df
Srikanth Kolisetty <82137255+srikanthkolisetty@users.noreply.github.com>
2023-07-12 16:02:31 +0530

    Add files via upload
    Main program is sort program

---
+++ DSA8.java
@@ -0,0 +1,34 @@
+package Ass19;
+
+import java.util.HashMap;
+
+public class DSA8 {
+
+
+	public static void main(String[] args) {
+		
+		int p1[]=new int[] {1,2,2,1};
+		int p2[] = new int[] {2,3};
+		int p3[]=new int[p1.length>p2.length?p2.length:p1.length];
+		HashMap<Integer,Integer> d = new HashMap();
+		int k=0;
+		for(int i=0;i<p1.length;i++)
+		{
+		
+				d.put(p1[i], 1);
+				for(int j=0;j<p2.length;j++)
+				{
+					if(p1[i]==p2[j])
+					{
+						p3[k]=p1[i];
+						k++;
+					}
+				}
+			}
+		
+		for(int i:p3)
+		{
+			System.out.println(i);
+		}
+	}
+}
